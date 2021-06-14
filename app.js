b41628995d8cfc792bf85c4efde2cfe848e9cd06
phanis07 <82591139+phanis07@users.noreply.github.com>
2021-06-14 13:34:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Updated to new nodejs app now.... update 2 servers Success  '));
+app.get('/', (req, res) => res.send('Hello World! Updated to new nodejs app now.... update 2 servers Success . BYE '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
